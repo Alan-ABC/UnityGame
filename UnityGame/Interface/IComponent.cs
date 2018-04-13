72516026a75a8f364b10c6cca7238f9b897efbab
Tim <37037147+TimQun@users.noreply.github.com>
2018-04-13 22:31:36 +0800

    增加基类对象组件管理

---
+++ UnityGame/Interface/IComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityGameToolkit
+{
+    public interface IComponent
+    {
+        void Initialize();
+
+        void Destroy();
+
+        void OnUpdate();
+        
+    }
+}
+
---
+++ UnityGame/Object/SmartObject.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UnityGameToolkit
+{
+    public class SmartObject : UObject
+    {
+        protected Dictionary<string, IComponent> mCompDic = new Dictionary<string, IComponent>();
+        protected List<IComponent> mCompList = new List<IComponent>();
+        protected void AddComponent(string compName, IComponent component)
+        {
+            if (!mCompDic.ContainsKey(compName))
+            {
+                mCompDic[compName] = component;
+                mCompList.Add(component);
+            }
+        }
+
+        protected void RemoveComponent(string compName)
+        {
+            if (mCompDic.ContainsKey(compName))
+            {
+                IComponent component = mCompDic[compName];
+                mCompList.Remove(component);
+                component.Destroy();
+            }
+        }
+
+        protected IComponent Retain(string compName)
+        {
+            if (mCompDic.ContainsKey(compName))
+            {
+                return mCompDic[compName];
+            }
+
+            return null;
+        }
+
+        protected void RemoveAllComponent()
+        {
+            foreach (KeyValuePair<string, IComponent> kvp in mCompDic)
+            {
+                kvp.Value.Destroy();
+            }
+
+            mCompDic.Clear();
+            mCompList.Clear();
+        }
+
+        protected virtual void Destroy()
+        {
+            RemoveAllComponent();
+        }
+
+        void Update()
+        {
+            for (int i = 0; i < mCompList.Count; ++i)
+            {
+                if (mCompList[i] != null)
+                {
+                    mCompList[i].OnUpdate();
+                }
+            }
+        }
+    }
+}
+
+
